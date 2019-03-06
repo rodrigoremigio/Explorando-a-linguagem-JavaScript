c976813c14024b6c42a586bc607487108fe7b5c8
Rodrigo Bezerra Remígio de Queiroz <rodrigo.brq@gmail.com>
2019-03-06 12:12:05 +0000

    Capítulo 08 concluído - Curso AlgaWorks

---
+++ Cap 08/8.03_disparando_eventos.js
@@ -0,0 +1,21 @@
+$(function(){
+	var atividade = $('#atividade');
+	var listaAtividades = $('#lista-atividades');
+
+	atividade.on('keyup', function(e){
+		var keyCode = e.keyCode;
+
+		if (keyCode === 13){
+			
+			atividade.trigger('enter', atividade.val());
+			atividade.val('');
+		}
+	}); 
+
+	atividade.on('enter', function(evento, texto) {
+
+		listaAtividades.prepend('<li>' + texto + '</li>');
+	});
+	
+
+});
---
+++ Cap 08/8.04_temporizadores.js
@@ -0,0 +1,8 @@
+$(function(){
+
+	setTimeout(function(){
+		$('#botao-clique-aqui').show();
+	}, 5000); //5 segundos
+
+
+});
